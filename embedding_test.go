d223236124135c1833ec286111dc32d4bdb66c3e
Chewxy <chewxy@gmail.com>
2021-01-27 06:05:49 +1100

    By indices embedding (#15)
    * Some work done on fc and norm
    
    * Updated Embedding to support ByIndices
    
    * Oops accidentally had replace directive in go.mod

--- embedding_test.go
+++ embedding_test.go
@@ -1,46 +1,83 @@
-package golgi
+package golgi_test
 
 import (
 	"fmt"
+	"log"
 
-	G "gorgonia.org/gorgonia"
+	. "gorgonia.org/golgi"
+	GG "gorgonia.org/gorgonia"
 	"gorgonia.org/qol"
 	"gorgonia.org/tensor"
 	"gorgonia.org/tensor/native"
 )
 
-//. "gorgonia.org/golgi"
+func testFn(emb, sel [][]float64, name string) {
+	for i := range sel {
+		var s int
+		if i == 1 {
+			s = 1
+		}
+		for j := range sel[i] {
+			if sel[i][j] != emb[s][j] {
+				fmt.Printf("%s error: Expected row %d to be equivalent to row %d of the embedding layer. Expected %v. Got %v instead", name, i, s, emb[s], sel[i])
+			}
+		}
+	}
 
-func ExampleEmbedding_1() {
+}
+
+func testFn3(mat [][][]float64, name string) {
+	for i := range mat {
+		for j := range mat[i] {
+			if i == 0 && j == 1 {
+				continue
+			}
+			var eq int
+			for k := range mat[i][j] {
+				if mat[i][j][k] == mat[0][1][k] { // strictly speaking we should use a better float cmp
+					eq++
+				}
+			}
+			if eq >= len(mat[i][j])/2 {
+				fmt.Printf("%s Row %d is the same as Row 1.\n%1.1f\n=\n%1.1f\n", name, j, mat[i][j], mat[0][1])
+			}
+		}
+
+	}
+}
+
+func ExampleEmbedding_Defaults() {
 	const (
 		N        = 13
 		dims     = 50
 		sentence = 10
 	)
-	g := G.NewGraph()
-	x := G.NewVector(g, qol.ClassType(), G.WithShape(sentence), G.WithInit(G.Zeroes()), G.WithName("sentence")) // WithInit or WithValue is required.
-	w := G.NewMatrix(g, tensor.Float64, G.WithShape(N, dims), G.WithName("embW"), G.WithInit(G.GlorotN(1)))
+	g := GG.NewGraph()
+	x := GG.NewVector(g, tensor.Int, GG.WithShape(sentence), GG.WithInit(GG.Zeroes()), GG.WithName("sentence")) // WithInit or WithValue is required.
+	w := GG.NewMatrix(g, tensor.Float64, GG.WithShape(N, dims), GG.WithName("embW"), GG.WithInit(GG.GlorotN(1)))
 	emb1 := NewEmbedding(WithWeights(w), WithName("emb1"), WithClasses(N), WithBatchSize(sentence))
 	emb2 := NewEmbedding(Of(tensor.Float64), WithSize(dims), WithClasses(N), WithBatchSize(sentence))
 	sel1 := emb1.Fwd(x)
 	sel2 := emb2.Fwd(x)
 
-	classes, _ := x.Value().Data().([]qol.Class)
+	classes := x.Value().Data().([]int)
 	classes[1] = 1
-
-	// Run() is required to be run everytime the VM is run.
-	// This is because the embedding layer relies on mechanisms external to the graphs' system.
-	if err := emb1.Run(x); err != nil {
-		fmt.Println(err)
-	}
-	if err := emb2.Run(x); err != nil {
-		fmt.Println(err)
-	}
-	vm := G.NewTapeMachine(g)
+	vm := GG.NewTapeMachine(g)
 	if err := vm.RunAll(); err != nil {
 		fmt.Println(err)
+		log.Fatal(err)
 	}
 	fmt.Printf("sel1: %v\nsel2: %v\n", sel1.Node().Shape(), sel2.Node().Shape())
+
+	emb1N, err := native.Matrix(emb1.Model()[0].Value().(*tensor.Dense))
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	emb2N, err := native.Matrix(emb2.Model()[0].Value().(*tensor.Dense))
+	if err != nil {
+		fmt.Println(err)
+	}
 
 	mat1, err := native.Matrix(sel1.Node().Value().(*tensor.Dense))
 	if err != nil {
@@ -53,24 +90,8 @@ func ExampleEmbedding_1() {
 	}
 
 	// only the 2nd word of the sentence doesn't have word ID 0
-	testFn := func(mat [][]float64, name string) {
-		for i := range mat {
-			if i == 1 {
-				continue
-			}
-			var eq int
-			for j := range mat[i] {
-				if mat[i][j] == mat[1][j] { // strictly speaking we should use a better float cmp
-					eq++
-				}
-			}
-			if eq >= len(mat[i])/2 {
-				fmt.Printf("%s Row %d is the same as Row 1.\n%1.1f\n=\n%1.1f\n", name, i, mat[i], mat[1])
-			}
-		}
-	}
-	testFn(mat1.([][]float64), "mat1")
-	testFn(mat2.([][]float64), "mat2")
+	testFn(emb1N.([][]float64), mat1.([][]float64), "mat1")
+	testFn(emb2N.([][]float64), mat2.([][]float64), "mat2")
 
 	// Output:
 	// sel1: (10, 50)
@@ -78,18 +99,82 @@ func ExampleEmbedding_1() {
 
 }
 
-func ExampleEmbedding_2() {
+func ExampleEmbedding_1() {
+	const (
+		N        = 13
+		dims     = 50
+		sentence = 10
+	)
+	g := GG.NewGraph()
+	x := GG.NewVector(g, qol.ClassType(), GG.WithShape(sentence), GG.WithInit(GG.Zeroes()), GG.WithName("sentence")) // WithInit or WithValue is required.
+	w := GG.NewMatrix(g, tensor.Float64, GG.WithShape(N, dims), GG.WithName("embW"), GG.WithInit(GG.GlorotN(1)))
+	emb1 := NewEmbedding(WithWeights(w), WithName("emb1"), WithClasses(N), WithBatchSize(sentence), AsRunner())
+	emb2 := NewEmbedding(Of(tensor.Float64), WithSize(dims), WithClasses(N), WithBatchSize(sentence), AsRunner())
+	sel1 := emb1.Fwd(x)
+	sel2 := emb2.Fwd(x)
+
+	classes := x.Value().Data().([]qol.Class)
+	classes[1] = 1
+
+	// Run() is required to be run everytime the VM is run.
+	// This is because the embedding layer relies on mechanisms external to the graphs' system.
+	if err := emb1.Run(x); err != nil {
+		fmt.Println(err)
+		log.Fatal(err)
+	}
+	if err := emb2.Run(x); err != nil {
+		fmt.Println(err)
+		log.Fatal(err)
+	}
+	vm := GG.NewTapeMachine(g)
+	if err := vm.RunAll(); err != nil {
+		fmt.Println(err)
+		log.Fatal(err)
+	}
+	fmt.Printf("sel1: %v\nsel2: %v\n", sel1.Node().Shape(), sel2.Node().Shape())
+
+	emb1N, err := native.Matrix(emb1.Model()[0].Value().(*tensor.Dense))
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	emb2N, err := native.Matrix(emb2.Model()[0].Value().(*tensor.Dense))
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	mat1, err := native.Matrix(sel1.Node().Value().(*tensor.Dense))
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	mat2, err := native.Matrix(sel2.Node().Value().(*tensor.Dense))
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	// only the 2nd word of the sentence doesn't have word ID 0
+	testFn(emb1N.([][]float64), mat1.([][]float64), "mat1")
+	testFn(emb2N.([][]float64), mat2.([][]float64), "mat2")
+
+	// Output:
+	// sel1: (10, 50)
+	// sel2: (10, 50)
+
+}
+
+func ExampleEmbedding_1_3Tensor() {
 	const (
 		N         = 13
 		dims      = 50
 		sentence  = 10
 		batchSize = 17
 	)
-	g := G.NewGraph()
-	x := G.NewMatrix(g, qol.ClassType(), G.WithShape(batchSize, sentence), G.WithInit(G.Zeroes()), G.WithName("sentence")) // WithInit or WithValue is required.
-	w := G.NewMatrix(g, tensor.Float64, G.WithShape(N, dims), G.WithName("embW"), G.WithInit(G.GlorotN(1)))
-	emb1 := NewEmbedding(WithWeights(w), WithName("emb1"), WithSize(dims), WithClasses(N), WithBatchSize(batchSize*sentence))
-	emb2 := NewEmbedding(Of(tensor.Float64), WithSize(dims), WithClasses(N), WithBatchSize(batchSize*sentence))
+	g := GG.NewGraph()
+	x := GG.NewMatrix(g, qol.ClassType(), GG.WithShape(batchSize, sentence), GG.WithInit(GG.Zeroes()), GG.WithName("sentence")) // WithInit or WithValue is required.
+	w := GG.NewMatrix(g, tensor.Float64, GG.WithShape(N, dims), GG.WithName("embW"), GG.WithInit(GG.GlorotN(1)))
+	emb1 := NewEmbedding(WithWeights(w), WithName("emb1"), WithSize(dims), WithClasses(N), WithBatchSize(batchSize*sentence), AsRunner())
+	emb2 := NewEmbedding(Of(tensor.Float64), WithSize(dims), WithClasses(N), WithBatchSize(batchSize*sentence), AsRunner())
 
 	sel1 := emb1.Fwd(x)
 	sel2 := emb2.Fwd(x)
@@ -105,7 +190,7 @@ func ExampleEmbedding_2() {
 	if err := emb2.Run(x); err != nil {
 		fmt.Println(err)
 	}
-	vm := G.NewTapeMachine(g)
+	vm := GG.NewTapeMachine(g)
 	if err := vm.RunAll(); err != nil {
 		fmt.Println(err)
 		return
@@ -122,28 +207,10 @@ func ExampleEmbedding_2() {
 	if err != nil {
 		fmt.Println(err)
 	}
-	// only the 2nd word of the sentence doesn't have word ID 0
-	testFn := func(mat [][][]float64, name string) {
-		for i := range mat {
-			for j := range mat[i] {
-				if i == 0 && j == 1 {
-					continue
-				}
-				var eq int
-				for k := range mat[i][j] {
-					if mat[i][j][k] == mat[0][1][k] { // strictly speaking we should use a better float cmp
-						eq++
-					}
-				}
-				if eq >= len(mat[i][j])/2 {
-					fmt.Printf("%s Row %d is the same as Row 1.\n%1.1f\n=\n%1.1f\n", name, j, mat[i][j], mat[0][1])
-				}
-			}
 
-		}
-	}
-	testFn(mat1, "mat1")
-	testFn(mat2, "mat2")
+	// only the 2nd word of the sentence doesn't have word ID 0
+	testFn3(mat1, "mat1")
+	testFn3(mat2, "mat2")
 
 	// Output:
 	// sel1: (17, 10, 50)
--- go.mod
+++ go.mod
@@ -2,20 +2,17 @@ module gorgonia.org/golgi
 
 go 1.15
 
+//replace gorgonia.org/gorgonia => ../gorgonia
+
 require (
-	github.com/apache/arrow/go/arrow v0.0.0-20201019013533-26aad5dc2281 // indirect
-	github.com/awalterschulze/gographviz v2.0.1+incompatible // indirect
 	github.com/chewxy/hm v1.0.0
 	github.com/chewxy/math32 v1.0.6
-	github.com/gogo/protobuf v1.3.1 // indirect
-	github.com/golang/protobuf v1.4.3 // indirect
-	github.com/google/flatbuffers v1.12.0 // indirect
-	github.com/leesper/go_rng v0.0.0-20190531154944-a612b043e353 // indirect
+	github.com/gogo/protobuf v1.3.2
+	github.com/golang/protobuf v1.4.3
 	github.com/pkg/errors v0.9.1
-	github.com/stretchr/testify v1.6.0
-	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
-	gonum.org/v1/gonum v0.8.1 // indirect
-	gorgonia.org/gorgonia v0.9.16-0.20201014112954-b44c85d30b88
+	github.com/stretchr/testify v1.7.0
+	gorgonia.org/gorgonia v0.9.16
 	gorgonia.org/qol v0.0.0-20201018204538-fc77242990ed
-	gorgonia.org/tensor v0.9.11
+	gorgonia.org/tensor v0.9.19
+
 )
